76d9d26d9e6cb0f66f80ed8b27e36153a1da8a71
Nikita ZIBOROV <Nikita.ZIBOROV@livenau.net>
2021-11-13 15:59:45 +0200

    Все сделал круто )

--- EssentialLesson11Task2/Car.cs
+++ EssentialLesson11Task2/Car.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EssentialLesson11Task2
+﻿namespace EssentialLesson11Task2
 {
     class Car
     {
